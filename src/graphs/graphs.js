af8ae1346bdd68d8ef4110089047f9531bdae732
Claude Jordan <claudejordan@gmail.com>
2024-05-24 11:10:44 +0200

    added functionality to create grid graphs, perform bfs

--- src/graphs/graphs.js
+++ src/graphs/graphs.js
@@ -20,4 +20,51 @@ class Graph {
   hasEdge(sourceNode, targetNode) {
     return this.graph[sourceNode].includes(targetNode);
   }
+
+  createGrid(height, width) {
+    // Create grid by inserting all nodes,
+    // then all edges to right and bottom
+    // of a given node (if applicable)
+   const totalNodes = height * width;
+
+    for (let i = 0; i < totalNodes; i++) {
+      this.addNode(i);
+    }
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const node = row * width + col;
+
+        if (col < width - 1) {
+          const rightNeighbor = node + 1;
+          this.addEdge(node, rightNeighbor);
+        }
+
+        if (row < height - 1) {
+          const bottomNeighbor = node + width;
+          this.addEdge(node, bottomNeighbor);
+        }
+      }
+    }
+  }
+
+  getNeighbours(node) {
+    return this.graph[node]
+  }
+
+  bfs(sourceNode, targetNode) {
+    let queue = [];
+    let parents = {};
+    queue.push(sourceNode);
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      if (currentNode === targetNode) return parents;
+      const neighbours = this.getNeighbours(currentNode);
+      for (let neighbour of neighbours) {
+        if (neighbour in parents) continue;
+        parents[neighbour] = currentNode;
+        queue.push(neighbour)
+      }
+    }
+  }
 }
